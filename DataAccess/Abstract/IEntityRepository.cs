eed577984e3fcb32d97eaa2b6eb640f2ee98cabb
Yiğit Ç <75942835+YigitCvk@users.noreply.github.com>
2021-03-14 17:30:28 +0300

    entity framework

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Entities.Abstract;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Abstract
@@ -8,7 +9,10 @@ namespace DataAccess.Abstract
     public interface IEntityRepository<T>where T:class,IEntity,new()
     {
 
-        List<T> GetById(int carId);
-        List<T> GetAll();
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        T Get(Expression<Func<T, bool>> filter);
     }
 }
---
+++ dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[CarId] INT NULL PRIMARY KEY, 
+    [BrandId] INT NULL, 
+    [ColorId] INT NULL, 
+    [ModelYear] INT NULL, 
+    [DailyPrices] DECIMAL NULL, 
+    [Description] VARCHAR(50) NULL
+)
